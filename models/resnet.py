761f632fc957e8d0c44b7cb5e0d86532a7887c0a
Xuanchen Yan <99523978+Stephen3zero24@users.noreply.github.com>
2022-04-03 00:21:13 +0800

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -1,3 +1,12 @@
+'''
+
+This file is copied from the following source:
+link: https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
+
+The original license is placed at the end of this file.
+
+'''
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -196,3 +205,29 @@ class Network(nn.Module):
         x = x.view(x.size(0), -1)
         out = self.fc(x)
         return x, out
+    
+'''
+
+MIT License
+
+Copyright (c) 2017 liukuang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+'''
