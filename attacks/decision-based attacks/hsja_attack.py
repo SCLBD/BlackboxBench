021c95bbbad7aac4c5ee08e2839943a639c2faa1
Xuanchen Yan <99523978+Stephen3zero24@users.noreply.github.com>
2022-04-02 13:18:44 +0800

    Update hsja_attack.py

--- attacks/decision-based attacks/hsja_attack.py
+++ attacks/decision-based attacks/hsja_attack.py
@@ -1,4 +1,10 @@
 '''
+
+This file is copied from the following source:
+link: https://github.com/cmhcbb/attackbox/blob/master/attack/HSJA.py
+
+The original license is placed at the end of this file.
+
 @inproceedings{chen2020hopskipjumpattack,
   title={Hopskipjumpattack: A query-efficient decision-based attack},
   author={Chen, Jianbo and Jordan, Michael I and Wainwright, Martin J},
@@ -8,7 +14,6 @@
   organization={IEEE}
 }
 
-rewrite from  https://github.com/cmhcbb/attackbox
 '''
 
 """
@@ -321,3 +326,31 @@ class HSJAttack(DecisionBlackBoxAttack):
         ys = ys_t.cpu().numpy()
         adv = self.hsja(xs, ys)
         return t(adv), self.query
+
+'''
+
+Original License
+
+MIT License
+
+Copyright (c) 2022 Minhao Cheng
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+'''
