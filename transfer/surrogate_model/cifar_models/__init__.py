5d7ed7f6169d30d5bd787c45a774880b4c01f862
郑 美曦 <223040254@link.cuhk.edu.cn>
2025-05-26 11:42:33 +0800

    Add files via upload

---
+++ transfer/surrogate_model/cifar_models/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+
+
+from .vgg import *
+from .resnext import *
+from .wrn import *
+from .densenet import *
+from .pyramidnet import *
+from .gdas import *
+from .resnet import *
+from .inceptionv3 import *
+from .adv_wrn_28_10 import *
---
+++ transfer/surrogate_model/cifar_models/gdas/lib/nas/__init__.py
@@ -0,0 +1,9 @@
+
+from .model_search    import Network
+from .CifarNet        import NetworkCIFAR
+from .ImageNet        import NetworkImageNet
+
+# genotypes
+from .genotypes       import model_types
+
+from .construct_utils import return_alphas_str
